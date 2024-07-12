af263b2719fd1357edb5fe2963ef17a0390b939d
peppapig450 <nbgenius1@gmail.com>
2024-07-12 12:25:28 -0700

    working metadata copier using pyexiftools's ExifToolAlpha

--- python/larpomatic.py
+++ python/larpomatic.py
@@ -1,66 +1,21 @@
-import pyexiv2
 import argparse
 from pprint import pprint
 from pathlib import Path
-from typing import Any
-from exiftool import ExifToolHelper
+import shlex
+import exiftool
 
 
-def print_out_metadata(source_path: str, target_path: str):
-    with ExifToolHelper() as et:
-        for d in et.get_metadata(source_path):
-            pprint(d)
+def get_json_metadata(et: exiftool.ExifTool, source_image):
+    params = ["-ee3", "-U", "-G3:1", "-api", "requestall=3", "-api", "largefilesupport"]
 
-        for d in et.get_metadata(target_path):
-            pprint(d)
+    data = et.execute_json(*params, source_image)
 
-
-def create_new_metadata(source_metadata: dict, target_metadata: dict):
-    remove_target_tags = {
-        "Exif.Image.Software",
-    }
-
-    # Remove the software key so that we can use the source's software key, and merge the keys
-    for key in remove_target_tags:
-        if target_metadata.get(key, None) is not None:
-            del target_metadata[key]
-
-    # Combine the two images metadata, target_metadata's keys take priority
-    # This is necessary so that the dimensions from the target image are used
-    new_metadata: dict[str, dict[str, str]] = source_metadata | target_metadata
-
-    return new_metadata
+    return data
 
 
-def copy_metadata(source_path, target_path):
-    source_path = Path(source_path).resolve()
-    target_path = Path(target_path).resolve()
-    source_metadata = {}
-    original_target_metadata = {}
-
-    pyexiv2.set_log_level(1)
-    # Load source and target images
-    with source_path.open("rb") as source_image:
-        with pyexiv2.ImageData(source_image.read()) as source_data:
-            source_metadata["exif"] = source_data.read_exif()
-            source_metadata["xmp"] = source_data.read_xmp()
-            source_metadata["iptc"] = source_data.read_iptc()
-
-    with target_path.open("rb") as target_image:
-        with pyexiv2.ImageData(target_image.read()) as target_data:
-            original_target_metadata.update(target_data.read_exif())
-            new_metadata = create_new_metadata(
-                source_metadata, original_target_metadata
-            )
-            target_data.modify_exif(new_metadata["exif"])
-            target_data.modify_xmp(new_metadata["xmp"])
-            target_data.modify_iptc(new_metadata["iptc"])
-
-    # Save the target image with the new metadata
-    # target_image.save(output_path)
-    # print(
-    #    f"Metadata copied from {source_path} to {target_path} and saved to {output_path}"
-    # )
+def copy_metadata_to_image(source_image, target_image):
+    with exiftool.ExifToolAlpha() as et:
+        et.copy_tags(source_image, target_image)
 
 
 def main():
@@ -80,7 +35,7 @@ def main():
     args = parser.parse_args()
 
     # copy_metadata(args.source, args.target)
-    print_out_metadata(args.source, args.target)
+    copy_metadata_to_image(args.source, args.target)
 
 
 if __name__ == "__main__":
